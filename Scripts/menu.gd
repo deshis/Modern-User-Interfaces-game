956fae310ff854d113572becdf5bb8c59a03e633
Altti Äyräs <altti.ayras@gmail.com>
2025-03-05 22:44:45 +0200

    credits button

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -4,14 +4,17 @@ extends Control
 #Play button
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
-	
+
 
 #Options button
 func _on_options_pressed() -> void:
 	pass 
 
 
-
 #Quit button
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
