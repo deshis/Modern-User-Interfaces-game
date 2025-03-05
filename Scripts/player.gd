7678e19a58261a9e38744cf4ef41df6fe4da25a5
Altti Äyräs <altti.ayras@gmail.com>
2025-03-05 22:42:09 +0200

    audio

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,11 +8,20 @@ var alive := true
 
 var game_started = false
 
+@onready var jump_audio_stream_player: AudioStreamPlayer2D = $JumpAudioStreamPlayer
+@onready var point_audio_stream_player: AudioStreamPlayer2D = $PointAudioStreamPlayer
+@onready var death_audio_stream_player: AudioStreamPlayer2D = $DeathAudioStreamPlayer
+
+
 func _ready() -> void:
 	velocity.y = JUMP_VELOCITY
 
+
 func start_game():
 	game_started = true
+	if alive: #???
+		jump_audio_stream_player.play()
+
 
 func _physics_process(delta: float) -> void:
 	if !game_started: 
@@ -23,6 +32,7 @@ func _physics_process(delta: float) -> void:
 	
 	if alive and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_VELOCITY
+		jump_audio_stream_player.play()
 	
 	velocity.x = 0
 	
@@ -31,15 +41,18 @@ func _physics_process(delta: float) -> void:
 	if not alive:
 		rotation += 0.15
 
+
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if alive:
 		if area.get_groups()[0] == "point":
 			score += 1
+			point_audio_stream_player.play()
 		elif area.get_groups()[0] == "enemy":
 			game_over()
 
+
 func game_over() -> void:
+	death_audio_stream_player.play()
 	alive = false
 	velocity.y = JUMP_VELOCITY
 	print("YOU DIED with a score of "+str(score))
-	
